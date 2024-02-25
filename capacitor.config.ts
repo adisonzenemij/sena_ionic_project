283b969c36c3b94dab1799305bbeb4bce86913f2
adisonzenemij <adisonzenemij@gmail.com>
2024-02-25 00:30:21 -0500

    Feat: Proyecto Desarrollado

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sena_ionic_project',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
